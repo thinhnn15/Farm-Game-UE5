3a7ad0d3d7080e2f4dee4868b3dc7536d16bd9f6
thinhnn15 <thinh.nguyenngoc15@gmail.com>
2025-12-23 10:51:17 +0700

    Add UFarmCropDataSubsystem to get the data from table

--- Source/MyFarm/Private/Core/Data/FarmCropDataSubsystem.cpp
+++ Source/MyFarm/Private/Core/Data/FarmCropDataSubsystem.cpp
@@ -8,6 +8,28 @@ const FCropTypeRow* UFarmCropDataSubsystem::GetCropData( FName CropRowId ) const
 {
     if ( !CropTypeDataTable )
         return nullptr;
-    
+
     return CropTypeDataTable->FindRow< FCropTypeRow >( CropRowId, TEXT( "UFarmCropDataSubsystem::GetCropTypeData" ) );
+}
+
+void UFarmCropDataSubsystem::Initialize( FSubsystemCollectionBase& Collection )
+{
+    Super::Initialize( Collection );
+
+    static const FString CropTablePath = TEXT( "/Game/MyFarm/Data/Crops/DT_CropTypes.DT_CropTypes" );
+    CropTypeDataTable = Cast< UDataTable >(
+        StaticLoadObject(
+            UDataTable::StaticClass(),
+            nullptr,
+            *CropTablePath
+        )
+    );
+
+    if ( !CropTypeDataTable )
+    {
+        UE_LOG( LogTemp, Error, TEXT("[FarmCropDataSubsystem] Failed to load CropTypeDataTable at path: %s"), *CropTablePath );
+        return;
+    }
+
+    UE_LOG( LogTemp, Log, TEXT("[FarmCropDataSubsystem] Successfully loaded CropTypeDataTable.") );
 }
