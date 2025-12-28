133f01c543e0b6aafd8b5ceb296dd7e9b9a51a32
thinhnn15 <thinh.nguyenngoc15@gmail.com>
2025-12-28 22:07:12 +0700

    Add FarmCharacter class

---
+++ Source/MyFarm/Private/Gameplay/Character/FarmCharacter.cpp
@@ -0,0 +1,45 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "Gameplay/Character/FarmCharacter.h"
+#include "Camera/CameraComponent.h"
+#include "GameFramework/SpringArmComponent.h"
+#include "GameFramework/CharacterMovementComponent.h"
+
+
+AFarmCharacter::AFarmCharacter()
+{
+    PrimaryActorTick.bCanEverTick = true;
+
+    // Character Rotation
+    // Character rotates based on movement direction
+    bUseControllerRotationYaw = false;
+
+    UCharacterMovementComponent* MoveComp = GetCharacterMovement();
+    if ( MoveComp )
+    {
+        MoveComp->bOrientRotationToMovement = true;
+        MoveComp->RotationRate = FRotator( 0.f, 540.f, 0.f );
+        MoveComp->MaxWalkSpeed = 400.f;
+    }
+    
+    // Camera setup
+    CameraBoom = CreateDefaultSubobject< USpringArmComponent >( TEXT( "CameraBoom" ) );
+    CameraBoom->SetupAttachment( RootComponent );
+    CameraBoom->TargetArmLength = 350.f;
+    CameraBoom->bUsePawnControlRotation = true;
+    
+    FollowCamera = CreateDefaultSubobject< UCameraComponent >( TEXT( "FollowCamera" ) );
+    FollowCamera->SetupAttachment( CameraBoom, USpringArmComponent::SocketName );
+    FollowCamera->bUsePawnControlRotation = false;
+}
+
+void AFarmCharacter::BeginPlay()
+{
+    Super::BeginPlay();
+}
+
+void AFarmCharacter::SetupPlayerInputComponent( UInputComponent* PlayerInputComponent )
+{
+    Super::SetupPlayerInputComponent( PlayerInputComponent );
+}
---
+++ Source/MyFarm/Public/Gameplay/Character/FarmCharacter.h
@@ -0,0 +1,33 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Character.h"
+#include "FarmCharacter.generated.h"
+
+class USpringArmComponent;
+class UCameraComponent;
+
+UCLASS()
+class MYFARM_API AFarmCharacter : public ACharacter
+{
+    GENERATED_BODY()
+
+public:
+    AFarmCharacter();
+
+protected:
+    virtual void BeginPlay() override;
+    
+    // Camera boom positioning the camera behind the character
+    UPROPERTY( VisibleAnywhere, BlueprintReadOnly, Category = "Camera" )
+    TObjectPtr< USpringArmComponent > CameraBoom;
+    
+    // Follow camera
+    UPROPERTY( VisibleAnywhere, BlueprintReadOnly, Category = "Camera" )
+    TObjectPtr< UCameraComponent > FollowCamera;
+
+public:
+    virtual void SetupPlayerInputComponent( class UInputComponent* PlayerInputComponent ) override;
+};
