f64a6d33c5bb5baf2b692b7272048fa65a2dc383
thinhnn15 <thinh.nguyenngoc15@gmail.com>
2026-01-02 19:59:56 +0700

    Add Story Subsysem function

---
+++ Source/MyFarm/Public/Gameplay/NPC/Story/NPCStorySubsystem.h
@@ -0,0 +1,16 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Subsystems/GameInstanceSubsystem.h"
+#include "NPCStorySubsystem.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class MYFARM_API UNPCStorySubsystem : public UGameInstanceSubsystem
+{
+    GENERATED_BODY()
+};
